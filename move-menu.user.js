3476cacba512ea06dc457304e801f67b13ed57cb
Isaiah Zwick-Schachter <isaiah@developingtechnician.com>
2017-09-18 18:14:09 -0400

    Moved flags and diamond to middle

--- move-menu.user.js
+++ move-menu.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Move Menu
-// @version      0.0.1
+// @version      0.0.2
 // @description  Move the important things to a gooder place
 // @author       thesecretmaster
 // @match        *.stackexchange.com*
@@ -14,4 +14,13 @@
     var rightSection = document.getElementsByClassName("-actions")[0];
     nav.remove();
     rightSection.insertBefore(nav, rightSection.childNodes[0]);
+
+    nav = nav.getElementsByClassName("-list")[0];
+    var diamond = document.getElementsByClassName("-item")[4];
+    var flags = document.getElementsByClassName("-item")[5];
+    diamond.remove();
+    flags.remove();
+    diamond.className = "-item";
+    nav.insertBefore(flags, nav.childNodes[0]);
+    nav.insertBefore(diamond, nav.childNodes[0]);
 })();
